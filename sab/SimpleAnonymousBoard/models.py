5b818799a54d2686447147f66216742210c7186b
biboy1999 <biboy1999@gmail.com>
2018-11-24 16:25:58 +0800

    db model created

--- sab/SimpleAnonymousBoard/models.py
+++ sab/SimpleAnonymousBoard/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Messages(models.Model):
+    MsgId = models.AutoField(primary_key=True)
+    Unixtime = models.IntegerField()
+    Username = models.CharField(max_length=20)
+    MessagesString = models.CharField(max_length=100)
