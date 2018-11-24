8b9d32ca6eee454aa81d4fca443ee648230011b3
Zeinok <zeinok@protonmail.com>
2018-11-24 21:40:09 +0800

    add views (untested)

--- sab/SimpleAnonymousBoard/views.py
+++ sab/SimpleAnonymousBoard/views.py
@@ -3,4 +3,35 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.shortcuts import redirect
+
+from SimpleAnonymousBoard.models import Message
+
+#import time
+
 # Create your views here.
+
+def viewMessages(request):
+    messages = Message.objects.all()
+    return render(request, 'templates/base.html', locals())
+
+def sendMessage(request):
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        if request.POST['msg'] != '':
+            hashlib = __import__('hashlib')
+            time = __import__('time')
+            inputName = request.POST['username']
+            username, password = inputname.split('#',1)
+            username = username[:12]
+            h = hashlib.sha512()
+            h.update(inputName)
+            h.update('howD0Y0uKn0wTheS@lt0w0')
+            msg = Message()
+            msg.Unixtime = int(time.time())
+            msg.Username = '%s#%s' % ( username, h.hexdigest()[7] )
+            msg.MessageString = request.POST['msg']
+            Message.objects.add(msg)
+            return redirect('/')
+
